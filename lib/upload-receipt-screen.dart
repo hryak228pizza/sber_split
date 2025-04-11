e7071056523b6cba66a7c8530c9824f04d059a85
hryak228pizza <I.Pavel-2004@mail.ru>
2025-04-11 22:57:01 +0300

    Update upload-receipt-screen.dart

--- lib/upload-receipt-screen.dart
+++ lib/upload-receipt-screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';  // Add this import
+import 'dart:io';
 
 
 class UploadReceiptScreen extends StatefulWidget {
