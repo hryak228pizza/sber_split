f7ac67141825e772e3b38166935de10c218d86dd
thisisaciidfree <plyusninavaleria@gmail.com>
2025-04-15 20:40:04 +0300

    change order provider import path

---
+++ lib/providers/OrderProvider.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:sber_split/services/image_service.dart';
+
+class ReceiptProvider extends ChangeNotifier {
+  final ImageService _imageService;
+  File? _image;
+
+  ReceiptProvider(this._imageService);
+
+  File? get image => _image;
+
+  Future<void> loadFromGallery() async {
+    _image = await _imageService.pickFromGallery();
+    notifyListeners();
+  }
+
+  Future<void> loadFromCamera() async {
+    _image = await _imageService.pickFromCamera();
+    notifyListeners();
+  }
+}
